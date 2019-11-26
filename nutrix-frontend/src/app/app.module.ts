a1044775d2172c36f93949aa30d677f872e14c9f
aboytsov <boytsov.andrew@gmail.com>
2019-11-26 19:58:39 +0300

    Init frontend project

---
+++ nutrix-frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './initial/app.component';
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
